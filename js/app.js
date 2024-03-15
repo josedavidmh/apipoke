67211bb4bcf05783ca426e07a86bbdd57943d697
josedavidmh <josedavidmontesino@yahoo.es>
2024-03-15 11:22:09 -0500

    primer commit

---
+++ js/app.js
@@ -0,0 +1,18 @@
+function buscar(){
+    document.getElementById("contenido").innerHTML=''
+    valor=document.getElementById("pokemon").value
+    cadena=""
+    //conexion a la api o ruta
+         url = "https://pokeapi.co/api/v2/pokemon/"+valor
+                   fetch(url)
+                  .then( response => response.json())
+                  .then( data =>  visualizar(data) )
+                  .catch( error => console.log(error))
+              const visualizar = (data => {
+                console.log(data)   
+                cadena=`<strong>Nombre:</strong> ${data.name} ` 
+                cadena+=`<img src=${data.sprites.front_default} width="300" height="300"></img>`
+                document.getElementById("contenido").innerHTML=cadena
+                
+       }  ) 
+    }
